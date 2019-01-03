65130f6568ef44a6ca5a755e3628d276b90d8dbf
João Henrique <jhbbortolotto@gmail.com>
2019-01-03 14:58:31 -0200

    feat: New module joke viewer.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,10 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.example.viewer.JokeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        new EndpointsAsyncTask().execute(this);
+        Intent startJokeActivity = new Intent(this, JokeActivity.class);
+
+        startActivity(startJokeActivity);
     }
 
 
